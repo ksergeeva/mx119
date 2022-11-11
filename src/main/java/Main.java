3d7bf38bbe5c6005efc87ee47774776f83336f41
ksergeeva <111127945+ksergeeva@users.noreply.github.com>
2022-11-11 19:25:39 +0300

    OSI

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Main {
+
+    private static final int PORT = 8989;
+    private static final String HOST = "localhost";
+
+    public static void main(String[] args) {
+        try (Socket clientSocket = new Socket(HOST, PORT);
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+            out.println("Client");
+            System.out.println(in.readLine());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
